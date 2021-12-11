d3630bd84f1ebccc8eff0eb276d1dad1508619c8
Rames Aliyev <ramesaliyev@gmail.com>
2021-12-11 22:06:56 +0300

    weighted-scheduling: add test functionality

--- projects/weighted-scheduling/solution/process.sh
+++ projects/weighted-scheduling/solution/process.sh
@@ -1,5 +1,29 @@
-# Compile program
+files=(
+  "./test/1.txt"
+  "./test/2.txt"
+)
+
+outputs=(
+  "8 | 1 3 5"
+  "16 | 1 4 6"
+)
+
+test() {
+  for i in ${!files[@]}; do
+    output=$(./main ${files[$i]})
+    
+    if [[ $output = ${outputs[$i]} ]]
+    then
+      echo "($i) OK [${files[$i]}]"
+    else
+      echo "($i) ERR {${files[$i]}} expected: [${outputs[$i]}] got: [$output]"
+    fi
+  done
+}
+
+# Always compile first.
 gcc main.c -std=c99 -pedantic -Wall -lm -o main
 
-# Run program
-./main
+# Main commands
+[[ $1 = "test" ]] && test # run tests
+[[ $1 != "test" ]] && ./main $1 # run program with arg
